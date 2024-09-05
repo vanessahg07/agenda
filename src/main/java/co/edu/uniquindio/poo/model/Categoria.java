19d3d199343cd96410d20758ad271dfad047aa97
vanessahg07 <vanehenaog05@gmail.com>
2024-09-04 20:09:09 -1100

    first commit

---
+++ src/main/java/co/edu/uniquindio/poo/model/Categoria.java
@@ -0,0 +1,7 @@
+package co.edu.uniquindio.poo.model;
+
+public enum Categoria {
+    OFICINA, FIESTA, AMIGOS, FAMILIA
+
+
+}
---
+++ src/main/java/co/edu/uniquindio/poo/model/Contacto.java
@@ -0,0 +1,101 @@
+package co.edu.uniquindio.poo.model;
+
+public class Contacto {
+    private String nombre;
+    private String alias;
+    private String direccion;
+    private String telefono;
+    private String email;
+    private int edad;
+
+    public Contacto(String nombre, String alias, String direccion, String telefono, String email){
+
+        this.nombre = nombre;
+        this.alias = alias;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAlias() {
+        return alias;
+    }
+
+    public void setAlias(String alias) {
+        this.alias = alias;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "\nContacto [nombre=" + nombre + ", alias=" + alias + ", direccion=" + direccion + ", telefono=" + telefono + ", email=" + email + "]";
+    }
+
+    /**
+     * @return the edad
+     */
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * @param edad the edad to set
+     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public int cuantasVocalesTiene(){
+
+        int cont = 0;     
+        for(int i = 0; i < nombre.length(); i++){
+        
+            if(esVocal(nombre.charAt(i))){
+               cont++;
+            }
+        }       
+        return cont;	
+    
+    }
+    
+    public boolean esVocal(char letra){
+    
+        if(letra == 'a' || letra == 'e'||letra == 'i'||letra == 'o'||letra == 'u'){
+          return true;
+        }else{
+            return false;
+        }
+    }
+    
+
+}
